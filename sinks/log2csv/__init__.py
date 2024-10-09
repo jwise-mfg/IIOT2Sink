54435abfcca51fbc53486868b105e9eef4b07a3f
jwise-mfg <jonathan.wise@cesmii.org>
2024-10-09 10:56:26 -0400

    tidy up

--- sinks/log2csv/__init__.py
+++ sinks/log2csv/__init__.py
@@ -11,9 +11,6 @@ class log2csv(sinks.sinkadapters):
         print("log2csv for real!")
 
     def write(timestamp, value, subscription):
-        print("log2csv is writing", value)
-        """
         with open('mqtt_log.csv', 'a') as f:
-        print ("Writing to mqtt_log.csv: ", str(value))
-        f.write(make_datetime_utc() + "," + label + "," + str(value) + "\r\n")
-        """
+            print ("Writing to mqtt_log.csv: ", str(value))
+            f.write(timestamp + "," + subscription.label + "," + str(value) + "\r\n")
--- start.py
+++ start.py
@@ -9,16 +9,16 @@ from pprint import pprint
 from sinks import sinkadapters
 
 if __name__ == '__main__':
-	for p in sinkadapters.sinks:
-		inst = p()
-		inst.start()
+    for p in sinkadapters.sinks:
+        inst = p()
+        inst.start()
 
 class subscription():
-	def __init__(self):
-		self.topic = None
-		self.member = None
-		self.sink = None
-		self.command = None
+    def __init__(self):
+        self.topic = None
+        self.member = None
+        self.sink = None
+        self.command = None
 
 #TODO: Load from a Config File
 mqtt_broker = "192.168.10.5"
@@ -41,48 +41,48 @@ sub2.label = "Solar Shed Temp"
 mqtt_subscriptions.append(sub2)
 
 def make_datetime_utc():
-	return datetime.now(timezone.utc).replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ') 
+    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ') 
 
 def search_json(data, member):
-	for key, value in data.items():
-		if key == member:
-			return value
+    for key, value in data.items():
+        if key == member:
+            return value
 
 def on_message(client, userdata, message):
-	msg = str(message.payload.decode("utf-8"))
-	print("Received MQTT message: ", msg)
-	# Check if there's sink configuration
-	for sub in mqtt_subscriptions:
-		if sub.topic == message.topic:
-			msg_sub = sub
-			if msg_sub.label == None:
-				msg_sub.label = msg_sub.topic
-	if msg_sub.member == None or msg_sub.member == "":
-		print ("No message member defined, using raw value")
-		value = msg
-	else:
-		# Check if the payload contains the configured member and parse out its avlue
-		print ("Searching for JSON payload member: " + msg_sub.member)
-		data = json.loads(msg)
-		#TODO: error handling
-		member_parts = msg_sub.member.split(".")
-		for member in member_parts:
-			data = search_json(data, member)
-		value = data
-		print ("Using discovered value:", value)
+    msg = str(message.payload.decode("utf-8"))
+    print("Received MQTT message: ", msg)
+    # Check if there's sink configuration
+    for sub in mqtt_subscriptions:
+        if sub.topic == message.topic:
+            msg_sub = sub
+            if msg_sub.label == None:
+                msg_sub.label = msg_sub.topic
+    if msg_sub.member == None or msg_sub.member == "":
+        print ("No message member defined, using raw value")
+        value = msg
+    else:
+        # Check if the payload contains the configured member and parse out its avlue
+        print ("Searching for JSON payload member: " + msg_sub.member)
+        data = json.loads(msg)
+        #TODO: error handling
+        member_parts = msg_sub.member.split(".")
+        for member in member_parts:
+            data = search_json(data, member)
+        value = data
+        print ("Using discovered value:", value)
 
-	# Check if we have a place to send the data
-	if isinstance(msg_sub.sink, list):
-		print ("using multiple sinks:", json.dumps(msg_sub.sink))
-	else:
-		print ("Using sink: ", msg_sub.sink)
-	
-	# Check if that requested sink adapter exists and write to it
-	for config_sink in msg_sub.sink:
-		for sink in sinkadapters.sinks:
-			if sink.name.lower() == config_sink.lower():
-				print ("sending to sink", sink.name)
-				sink.write(make_datetime_utc(), value, msg_sub)
+    # Check if we have a place to send the data
+    if isinstance(msg_sub.sink, list):
+        print ("using multiple sinks:", json.dumps(msg_sub.sink))
+    else:
+        print ("Using sink: ", msg_sub.sink)
+    
+    # Check if that requested sink adapter exists and write to it
+    for config_sink in msg_sub.sink:
+        for sink in sinkadapters.sinks:
+            if sink.name.lower() == config_sink.lower():
+                print ("sending to sink", sink.name)
+                sink.write(make_datetime_utc(), value, msg_sub)
 
 print("Using paho-mqtt version: ", paho.mqtt.__version__)
 if paho.mqtt.__version__[0] > '1':
@@ -92,7 +92,7 @@ else:
 
 client.connect(mqtt_broker)
 for sub in mqtt_subscriptions:
-	print("subscribing to: ", sub.topic)
-	client.subscribe(str(sub.topic))
+    print("subscribing to: ", sub.topic)
+    client.subscribe(str(sub.topic))
 client.on_message=on_message
 client.loop_forever()
